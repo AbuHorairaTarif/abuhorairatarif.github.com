552f18517b2b8e1269894402db7bbf50e0a91ee4
= <=>
2023-12-10 21:51:23 +0600

    custom animation added and remove library

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -4,9 +4,7 @@ const navMenuMb = document.getElementById("navbar-mb-ul");
 const closeIcon = document.getElementById("close-icon");
 
 window.addEventListener("scroll", function(){
-    navbar.classList.toggle("sticky", window.scrollY>10);
-
-    
+    navbar.classList.toggle("sticky", window.scrollY>10);    
 })
 
 hamburgerIcon.addEventListener("click", function(){
@@ -23,6 +21,22 @@ closeIcon.addEventListener("click", function(){
     navMenuMb.style.transition = "0.7s all";
     navMenuMb.style.right = "-100%";
     navMenuMb.style.opacity = "0";
-    
-
 })
+
+window.addEventListener('scroll',reveal);
+
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+    for(var i=0;i<reveals.length;i++) {
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if(revealtop < windowheight - revealpoint){
+            reveals[i].classList.add('activate');
+        }
+        else{
+            reveals[i].classList.remove('activate');
+        }
+    }
+}
